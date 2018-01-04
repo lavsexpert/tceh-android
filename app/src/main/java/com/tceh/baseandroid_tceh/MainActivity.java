1632a0c2cb08acd2ebccc3e4c8ff385e036cb94e
Lavsexpert <lavrov-sergey@yandex.ru>
2018-01-05 10:55:24 +1100

    Создание проекта и репозитария по разработке под Android по курсу в tceh

---
+++ app/src/main/java/com/tceh/baseandroid_tceh/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tceh.baseandroid_tceh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
